6a45e095b26e3b3d4271ecc9b5853c99e8883871
Nirvana <isnirvana788@gmail.com>
2025-05-01 00:55:33 +0000

    feat: render message on page based on the user input

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
 import OpenAI from "openai"
+import logo from "../assets/icons/logo.png"
 
 const input = document.getElementById("input")
 const main = document.querySelector("main")
@@ -35,27 +36,98 @@ async function fetchData() {
   })
 
   const result = completion.choices[0].message
+
   const assistant = {
     role: "assistant",
     content: result.content,
   }
   messages.push(assistant)
 
-  return messages
+  return { message: result.content, userInput: userInput }
 }
 
 function getInputValue() {
   const inputValue = input.value
 
-  if (!inputValue) {
-    alert("Please type something...")
-  }
   return inputValue
+}
+
+async function renderOnPage() {
+  try {
+    const results = await fetchData()
+    const message = results.message
+
+    marked.setOptions({
+      highlight: function (code, lang) {
+        const language = hljs.getLanguage(lang) ? lang : "plaintext"
+        return hljs.highlight(code, { language }).value
+      },
+    })
+
+    const html = marked.parse(message)
+    console.log(results)
+    createAssistantHTML(html)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function createUserHTML() {
+  const user = createElement("div", "user")
+
+  user.innerHTML = `
+      <div class="user__massage">
+          <div class="message-text">
+            ${input.value}
+          </div>
+          <div class="user__profile"></div>
+        </div>
+    `
+
+  main.appendChild(user)
+}
+
+function createAssistantHTML(html) {
+  const assistant = createElement("div", "assistant")
+
+  assistant.innerHTML = `
+    <div class="assistant__massage">
+          <div class="message-text">
+            ${html}
+          </div>
+          <hr />
+          <div class="assistant__massage--icons">
+            <i class="fa-regular fa-copy"></i>
+            <i class="fa-solid fa-arrows-rotate"></i>
+          </div>
+        </div>
+        <div class="assistant__profile">
+          <img src="${logo}" alt="t" />
+        </div>
+  `
+
+  main.appendChild(assistant)
+}
+
+function createElement(tag, className) {
+  const element = document.createElement(tag)
+  element.classList = className
+
+  return element
 }
 
 main.addEventListener("click", async (e) => {
   const target = e.target
 
   if (!target.classList.contains("fa-arrow-up")) return
-  fetchData()
+  createUserHTML()
+  renderOnPage()
+})
+
+document.addEventListener("keydown", (e) => {
+  if (e.key !== "Enter") return
+  console.log(e.key)
+  createUserHTML()
+  renderOnPage()
+  input.value = ""
 })
